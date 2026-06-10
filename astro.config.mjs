325fa0b6f3857d7adf3ec2318c2ae5d168e06498
denden <69514964+dendend12345@users.noreply.github.com>
2026-06-10 16:46:16 +0900

    fix icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     host: true,
     site: 'https://<username>.github.io',
-    base: '/repo-name', // リポジトリ名をスラッシュで囲む
+    base: '/DenDen-HP', // リポジトリ名をスラッシュで囲む
     watch: {
       usePolling: true,
       interval: 100,
