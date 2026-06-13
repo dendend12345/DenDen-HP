18df2a1d34d22789edd3d1e1c4924429c72cb5f2
denden <69514964+dendend12345@users.noreply.github.com>
2026-06-13 12:01:50 +0900

    use backup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,13 @@
 import { defineConfig } from 'astro/config';
-// import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   server: {
     host: true,
-    site: 'https://dendend12345.com',
-    //base: '/DenDen-HP', // リポジトリ名をスラッシュで囲む
-    base: '/',
+    site: 'https://<username>.github.io',
+    base: '/DenDen-HP', // リポジトリ名をスラッシュで囲む
     watch: {
       usePolling: true,
       interval: 100,
     },
   },
-
-  integrations: [
-    /*
-    partytown({
-      config: {
-        lib: '/*partytown',
-        forward: ['gtag', 'dataLayer.push'],
-      },
-    }),
-    */
-  ],
-
 });
