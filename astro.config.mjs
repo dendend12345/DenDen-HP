f97c85f6484a706551e44c96c82bb6243d27fe59
denden <2dendenden2@gmail.com>
2026-09-07 18:53:18 +0900

    fix animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
+import { defineConfig } from 'astro/config';
 import typography from '@tailwindcss/typography';
 
+import tailwind from '@astrojs/tailwind';
+
 /** @type {import('tailwindcss').Config} */
-export default {
+export default defineConfig({
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
   theme: {
     extend: {},
   },
+
   plugins: [
     typography,
   ],
-};
+
+  integrations: [tailwind()],
+});
